a05786b866562f1eabac57878c50b24c2d5200bf
Matheus Pitsch <101012809+MatheusPitsch@users.noreply.github.com>
2022-11-01 11:06:49 -0300

    creating ef core

---
+++ Projeto/CasaDoCodigo/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(l => l.Id);
+        }
+    }
+}
