c6dc7a2515909c75c7c249c343e9f0373832d687
Matheus Pitsch <101012809+MatheusPitsch@users.noreply.github.com>
2022-11-01 16:08:58 -0300

    ensuring the database is created

--- Projeto/CasaDoCodigo/Startup.cs
+++ Projeto/CasaDoCodigo/Startup.cs
@@ -33,7 +33,7 @@ namespace CasaDoCodigo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider service)
         {
             if (env.IsDevelopment())
             {
